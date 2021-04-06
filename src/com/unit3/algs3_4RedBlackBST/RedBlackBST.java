c92823c3d83ae2ee9990cd51df6ce6eec8db2a9b
Kou <kou1688@foxmail.com>
2021-04-06 14:40:03 +0800

    Algorithm

--- src/com/unit3/algs3_4RedBlackBST/RedBlackBST.java
+++ src/com/unit3/algs3_4RedBlackBST/RedBlackBST.java
@@ -1,4 +1,26 @@
 package com.unit3.algs3_4RedBlackBST;
 
-public class RedBlackBST {
+/**
+ * 红黑树
+ *
+ * @author Kou
+ */
+public class RedBlackBST<Key extends Comparable<Key>, Value> {
+    private static final boolean RED = true;
+    private static final boolean BLACK = false;
+
+    private class Node {
+        private Key key;
+        private Value val;
+        private Node left, right;
+        private int N;
+        private boolean color;
+
+        public Node(Key key, Value val, boolean color, int N) {
+            this.key = key;
+            this.val = val;
+            this.color = color;
+            this.N = N;
+        }
+    }
 }
